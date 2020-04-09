5f0c0129a9c50374eb7c6c3e78b9f666997a6365
alexlecco <alexlecco24@gmail.com>
2020-04-09 13:11:44 -0300

    create reducer

---
+++ src/js/reducers/index.js
@@ -0,0 +1,12 @@
+// state inicial
+const initialState = {
+    articles: []
+};
+  
+// unico reducer que tenemos (aun no hace nada)
+function rootReducer(state = initialState, action) {
+    return state;
+};
+
+// exportamos el reducer
+export default rootReducer;
